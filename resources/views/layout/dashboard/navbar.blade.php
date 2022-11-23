cf7c93b7e60c61a474555d3db715018ce6158498
auryno19 <aurynoaga19@gmail.com>
2022-11-23 09:29:48 +0700

    testing

--- resources/views/layout/dashboard/navbar.blade.php
+++ resources/views/layout/dashboard/navbar.blade.php
@@ -1,4 +1,4 @@
-<nav class="navbar default-layout col-lg-12 col-12 p-0 fixed-top d-flex align-items-top flex-row">
+{{-- <nav class="navbar default-layout col-lg-12 col-12 p-0 fixed-top d-flex align-items-top flex-row">
     <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-start">
       <div class="me-3">
         <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-bs-toggle="minimize">
@@ -18,7 +18,7 @@
       <ul class="navbar-nav">
         <li class="nav-item font-weight-semibold d-none d-lg-block ms-0">
           <h1 class="welcome-text">Hello, <span class="text-black fw-bold">{{ auth()->user()->name }}</span></h1>
-          {{-- <h3 class="welcome-sub-text">Your performance summary this week </h3> --}}
+          
         </li>
       </ul>
       <ul class="navbar-nav ms-auto">
@@ -27,7 +27,7 @@
             <img class="img-xs rounded-circle" src="{{ asset('images/user.png') }}" alt="Profile image"> </a>
           <div class="dropdown-menu dropdown-menu-right navbar-dropdown" aria-labelledby="UserDropdown">
             <div class="dropdown-header text-center">
-              {{-- <img class="img-md rounded-circle" src="{{ asset('images/faces/face8.jpg') }}" alt="Profile image"> --}}
+             
               <p class="mb-1 mt-3 font-weight-semibold">{{ auth()->user()->name }}</p>
               <p class="fw-light text-muted mb-0">{{ auth()->user()->email }}</p>
             </div>
@@ -36,12 +36,12 @@
               @csrf
               <button type="submit"  class="dropdown-item"><i class="dropdown-item-icon mdi mdi-power text-primary me-2"></i>Logout</button>
             </form>
-            {{-- <a href="" class="dropdown-item"><i class="dropdown-item-icon mdi mdi-power text-primary me-2"></i>Sign Out</a> --}}
+            
           </div>
         </li>
       </ul>
       <button class="navbar-toggler navbar-toggler-right d-lg-none align-self-center" type="button" data-bs-toggle="offcanvas">
         <span class="mdi mdi-menu"></span>
       </button>
-    </div>
+    </div> --}}
   </nav>
